83f8d610955aa19fd7f7922827f1e65cc9b8d780
themonomers <54368885+themonomers@users.noreply.github.com>
2020-07-05 14:06:26 -0700

    Script to backup keepass db on crontab

---
+++ backup_keepass.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# backup copy of shared Keepass databases if different 
+FILE=$(ls -t main* | head -1)
+STR=$(diff -s "$FILE" /mnt/gdrive/misc/main.kdbx)
+SUB='identical'
+if [[ "$STR" != *"$SUB"* ]]; then
+  cp /mnt/gdrive/misc/main.kdbx /home/pi/keepass/main.$(date +%Y.%m.%d.%H.%M.%S).kdbx
+fi
+
+FILE=$(ls -t other* | head -1)
+STR=$(diff -s "$FILE" /mnt/gdrive_other/other.kdbx)
+if [[ "$STR" != *"$SUB"* ]]; then
+  cp /mnt/gdrive_other/other.kdbx /home/pi/keepass/other.$(date +%Y.%m.%d.%H.%M.%S).kdbx
+fi
+
+# backup the backups to usb drive
+rsync -a --delete /home/pi/keepass /mnt/usb
