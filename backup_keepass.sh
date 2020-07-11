809f344d6b8231942f57797f594cf091b6e7aeed
themonomers <54368885+themonomers@users.noreply.github.com>
2020-07-11 09:42:33 -0700

    Fixed path for ls

--- backup_keepass.sh
+++ backup_keepass.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # backup copy of shared Keepass databases if different 
-FILE=$(ls -t main* | head -1)
+FILE=$(ls -t /home/pi/keepass/main* | head -1)
 STR=$(diff -s "$FILE" /mnt/gdrive/misc/main.kdbx)
 SUB='differ'
 if [[ "$STR" == *"$SUB"* ]]; then
   cp /mnt/gdrive/misc/main.kdbx /home/pi/keepass/main.$(date +%Y.%m.%d.%H.%M.%S).kdbx
 fi
 
-FILE=$(ls -t other* | head -1)
-STR=$(diff -s "$FILE" /mnt/gdrive_other/other.kdbx)
+FILE=$(ls -t /home/pi/keepass/kathy* | head -1)
+STR=$(diff -s "$FILE" /mnt/gdrive_kathy/kathy.kdbx)
 if [[ "$STR" == *"$SUB"* ]]; then
-  cp /mnt/gdrive_other/other.kdbx /home/pi/keepass/other.$(date +%Y.%m.%d.%H.%M.%S).kdbx
+  cp /mnt/gdrive_kathy/kathy.kdbx /home/pi/keepass/kathy.$(date +%Y.%m.%d.%H.%M.%S).kdbx
 fi
 
 # backup the backups to usb drive
