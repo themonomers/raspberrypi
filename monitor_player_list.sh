dad18f9532ec396c314a261bf1c19ef35e519d78
themonomers <54368885+themonomers@users.noreply.github.com>
2020-08-08 13:37:27 -0700

    Output screen commands to /dev/null
    Fix condition when screen is not running.  Increased column width to 500 to include longer list of players.

--- monitor_player_list.sh
+++ monitor_player_list.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #send list players command to minecraft screen
-screen -Rd minecraft -p 0 -X width -d 300
-screen -Rd minecraft -p 0 -X stuff "list $(printf '\r')"
+screen -Rd minecraft -p 0 -X width -d 500 > /dev/null 2>&1
+screen -Rd minecraft -p 0 -X stuff "list $(printf '\r')" > /dev/null 2>&1
 
 #pause to allow command to execute and return output before writing to file
 sleep 1
-screen -Rd minecraft -p 0 -X hardcopy
+screen -Rd minecraft -p 0 -X hardcopy > /dev/null 2>&1
 
 #grab the last output line from the list command and parse out player names
 STR=$(tac /home/pi/Paper/hardcopy.0 | grep -m1 INFO)
