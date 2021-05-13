af747f03ba71be0c9658b77cdac94fbb3c6e3184
themonomers <54368885+themonomers@users.noreply.github.com>
2021-05-12 23:23:52 -0700

    Create backup_influxdb.sh

---
+++ backup_influxdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# backup InfluxDB into a new folder 
+DIR=influx-$(date +%Y.%m.%d.%H.%M.%S)
+mkdir /home/pi/influxdb-backup/$DIR
+cd /home/pi/influxdb-backup/$DIR
+influxd backup -portable .
+
+# delete all but the last 5 backup folders
+#ls -t influx-* | tail -n +6 | xargs rm --
+ls -t -d /home/pi/influxdb-backup/influx-* | tail -n +6 | xargs rm -rf 
+
+# backup the backups to usb drive
+rsync -a --delete /home/pi/influxdb-backup /mnt/usb
