158ea0f2f66c2b6536a602cdb13e6a200b53bd55
themonomers <54368885+themonomers@users.noreply.github.com>
2021-05-13 09:27:20 -0700

    Add trimming to number of backups.

--- backup_keepass.sh
+++ backup_keepass.sh
@@ -8,11 +8,17 @@ if [[ "$STR" == *"$SUB"* ]]; then
   cp /mnt/gdrive/misc/main.kdbx /home/pi/keepass/main.$(date +%Y.%m.%d.%H.%M.%S).kdbx
 fi
 
+# delete all but the last 10 files
+ls -t kathy.* | tail -n +11 | xargs rm --
+
 FILE=$(ls -t /home/pi/keepass/kathy* | head -1)
 STR=$(diff -s "$FILE" /mnt/gdrive_kathy/kathy.kdbx)
 if [[ "$STR" == *"$SUB"* ]]; then
   cp /mnt/gdrive_kathy/kathy.kdbx /home/pi/keepass/kathy.$(date +%Y.%m.%d.%H.%M.%S).kdbx
 fi
+
+# delete all but the last 10 files
+ls -t main.* | tail -n +11 | xargs rm --
 
 # backup the backups to usb drive
 rsync -a --delete /home/pi/keepass /mnt/usb
