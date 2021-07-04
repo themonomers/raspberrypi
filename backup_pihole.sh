9f16ff26b15935c336210788e2211b9a08421415
themonomers <54368885+themonomers@users.noreply.github.com>
2021-07-04 09:11:10 -0700

    Added file path to Pihole command.

--- backup_pihole.sh
+++ backup_pihole.sh
@@ -2,7 +2,7 @@
 
 # backup pihole teleporter into usb drive
 cd /mnt/usb/pihole
-pihole -a -t
+/usr/local/bin/pihole -a -t
 
 # delete all but the last 5 backups
 ls -t -d /mnt/usb/pihole/pi-hole-teleporter* | tail -n +6 | xargs rm -rf 
