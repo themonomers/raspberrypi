89dc7b8da9be45720aef998168bf2b6e5b1d2d71
themonomers <54368885+themonomers@users.noreply.github.com>
2021-07-07 11:22:18 -0700

    Updated file paths.
    Included rsync to usb drive.

--- pihole/backup_pihole.sh
+++ pihole/backup_pihole.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # backup pihole teleporter into usb drive
-cd /mnt/usb/pihole
+cd /home/pi/pihole
 /usr/local/bin/pihole -a -t
 
 # delete all but the last 5 backups
-ls -t -d /mnt/usb/pihole/pi-hole-teleporter* | tail -n +6 | xargs rm -rf 
+ls -t -d /home/pi/pihole/pi-hole-teleporter* | tail -n +6 | xargs rm -rf 
+
+# backup the backups to usb drive
+rsync -a --delete /home/pi/pihole /mnt/usb
