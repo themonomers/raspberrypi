74f315f62a10bb0b894378939f7df718fb9ca289
themonomers <54368885+themonomers@users.noreply.github.com>
2022-08-31 09:03:32 -0700

    Added automated backup of dnsmasq.conf

--- pihole/backup_pihole.sh
+++ pihole/backup_pihole.sh
@@ -4,8 +4,13 @@
 cd /home/pi/pihole
 /usr/local/bin/pihole -a -t
 
+# copy dns mapping file
+timestamp=$(date +"%Y.%m.%d.%H.%M.%S")
+sudo cp /etc/dnsmasq.conf /home/pi/pihole/dnsmasq-$timestamp.conf
+
 # delete all but the last 5 backups
 ls -t -d /home/pi/pihole/pi-hole-teleporter* | tail -n +6 | xargs rm -rf 
+ls -t -d /home/pi/pihole/dnsmasq-* | tail -n +6 | xargs rm -rf
 
 # backup the backups to usb drive
 rsync -a --delete /home/pi/pihole /mnt/usb
