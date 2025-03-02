cff463f24d68847b48457c8184c507b83b9614d5
themonomers <54368885+themonomers@users.noreply.github.com>
2025-03-01 20:27:20 -0800

    Updates for Pihole v6
    Different command for command line Teleporter and removed lines for dnsmasq as these are now configured in Pihole Admin and backed up in Teleporter.

--- pihole/backup_pihole.sh
+++ pihole/backup_pihole.sh
@@ -2,20 +2,10 @@
 
 # run pihole teleporter to backup configuration as an archive
 cd /home/pi/pihole
-/usr/local/bin/pihole -a -t
-
-# backup DNS mapping file if different
-FILE=$(ls -t /home/pi/pihole/dnsmasq-* | head -1)
-STR=$(diff -s -q "$FILE" /etc/dnsmasq.conf)
-SUB='differ'
-if [[ "$STR" == *"$SUB"* ]]; then
-  timestamp=$(date +"%Y.%m.%d.%H.%M.%S")
-  sudo cp /etc/dnsmasq.conf /home/pi/pihole/dnsmasq-$timestamp.conf
-fi
+sudo pihole-FTL --teleporter
 
 # delete all but the last 5 backups
-ls -t -d /home/pi/pihole/pi-hole-teleporter* | tail -n +6 | xargs rm -rf 
-ls -t -d /home/pi/pihole/dnsmasq-* | tail -n +6 | xargs rm -rf
+ls -t -d /home/pi/pihole/pi-hole_teleporter* | tail -n +6 | xargs rm -rf
 
 # backup the backups to usb drive
 rsync -a --delete /home/pi/pihole /mnt/usb
