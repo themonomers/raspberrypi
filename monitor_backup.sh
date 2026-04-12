05d03651ae0f4d79a82fd20ac0f81d9e14d3cba5
themonomers <54368885+themonomers@users.noreply.github.com>
2026-04-12 11:02:50 -0700

    Create monitor_backup.sh

---
+++ monitor_backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+GRAFANA_BACKED_UP=false
+DEV_BACKED_UP=false
+
+TODAY=$(date +%F)
+#echo $TODAY
+
+# check file date
+cd /mnt/usb/grafana
+FILE=$(ls -1t | sed -n '2p')
+FILE_DATE=$(stat -c %y "$FILE" | cut -d' ' -f1)
+#echo $FILE_DATE
+
+if [[ $TODAY == $FILE_DATE ]]; then
+  GRAFANA_BACKED_UP=true
+fi  
+
+# check file date
+cd /mnt/usb/dev/tesla/python
+FILE=$(ls -t | head -n 1)
+FILE_DATE=$(stat -c %y "$FILE" | cut -d' ' -f1)
+#echo $FILE_DATE
+
+if [[ $TODAY == $FILE_DATE ]]; then
+  DEV_BACKED_UP=true
+fi
+
+# send notification if back up failed
+if [[ $GRAFANA_BACKED_UP == "false" ]] || [[ $DEV_BACKED_UP == "false" ]]; then
+  curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data '{"content":"'"One or more backups failed:  \n  Grafana:  $GRAFANA_BACKED_UP\n  Dev:  $DEV_BACKED_UP\n"'"}' $webhook &> /dev/null
+fi
