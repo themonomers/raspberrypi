a08b4f24f21e84ff617a5d8ecff6caf58739e33c
themonomers <54368885+themonomers@users.noreply.github.com>
2026-04-12 13:38:31 -0700

    Create monitor_backup.sh

---
+++ pihole/monitor_backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PIHOLE_BACKED_UP=false
+
+TODAY=$(date +%F)
+#echo $TODAY
+
+# check file date
+cd /mnt/usb/pihole
+FILE=$(ls -1t | sed -n '2p')
+FILE_DATE=$(stat -c %y "$FILE" | cut -d' ' -f1)
+#echo $FILE_DATE
+
+if [[ $TODAY == $FILE_DATE ]]; then
+  PIHOLE_BACKED_UP=true
+fi
+
+# send notification if back up failed
+if [[ $PIHOLE_BACKED_UP == "false" ]]; then
+  curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data '{"content":"'"One or more backups failed:  \n  Pihole:  $PIHOLE_BACKED_UP"'"}' $webhook &> /dev/null
+fi
