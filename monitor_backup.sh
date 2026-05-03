9ad13aa7dece3d04961bc0d812e0e51e3a9902ed
themonomers <54368885+themonomers@users.noreply.github.com>
2026-05-03 10:57:09 -0700

    Check backups of all subfolders

--- monitor_backup.sh
+++ monitor_backup.sh
@@ -18,12 +18,25 @@ fi
 
 # check file date
 cd /mnt/usb/dev/tesla/python/test/common
-FILE=$(ls -t | head -n 1)
-FILE_DATE=$(stat -c %y "$FILE" | cut -d' ' -f1)
-#echo $FILE_DATE
+FILE_COMMON=$(ls -t | head -n 1)
+FILE_DATE_COMMON=$(stat -c %y "$FILE_COMMON" | cut -d' ' -f1)
+#echo $FILE_DATE_COMMON
 
-if [[ $TODAY == $FILE_DATE ]]; then
+cd /mnt/usb/dev/tesla/python/test/energy
+FILE_ENERGY=$(ls -t | head -n 1)
+FILE_DATE_ENERGY=$(stat -c %y "$FILE_ENERGY" | cut -d' ' -f1)
+#echo $FILE_DATE_ENERGY
+
+cd /mnt/usb/dev/tesla/python/test/vehicle
+FILE_VEHICLE=$(ls -t | head -n 1)
+FILE_DATE_VEHICLE=$(stat -c %y "$FILE_VEHICLE" | cut -d' ' -f1)
+#echo $FILE_DATE_VEHICLE
+
+if [[ $TODAY == $FILE_DATE_COMMON ]] &&
+   [[ $TODAY == $FILE_DATE_ENERGY ]] &&
+   [[ $TODAY == $FILE_DATE_VEHICLE ]]; then
   DEV_BACKED_UP=true
+#  echo "true"
 fi
 
 # send notification if back up failed
