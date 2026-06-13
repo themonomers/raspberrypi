8ee9135cb7c022bee6a757fea650133aca13900a
themonomers <54368885+themonomers@users.noreply.github.com>
2026-06-13 15:57:23 -0700

    Create run_tesla_command_proxy.sh

---
+++ vehicle-command/run_tesla_command_proxy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/pi/vehicle-command
+
+/usr/local/go/bin/go run /home/pi/vehicle-command/cmd/tesla-http-proxy -tls-key /home/pi/tesla/python/secrets/key.pem -cert /home/pi/tesla/python/secrets/cert.pem -port 4443 -key-file /home/pi/tesla/python/secrets/tesla_private_key.pem > tesla-command-proxy.log 2>&1 &
